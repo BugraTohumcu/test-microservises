dd9357aa02475c5ae73a5b11c0f832072116c3bc
Ahmet Buğra Tohumcu <tohumcuahmetbugra@gmail.com>
2025-06-30 11:49:01 +0300

    User initilaied

--- User/src/index.ts
+++ User/src/index.ts
@@ -1,11 +1,18 @@
 import express from 'express';
 import { Request , Response } from 'express';
-
-
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.get('/user',(req: Request,res: Response) => {
-    res.send('User Service');
+    res.json({message: 'User Service'});
+});
+
+
+app.listen(PORT , () => {
+    console.log(`The user-server is running at http://localhost:${PORT}`);
+    
 });
