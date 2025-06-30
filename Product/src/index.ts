ac56ca40ab9da062ca685a6533f082f903dd8310
Ahmet Buğra Tohumcu <tohumcuahmetbugra@gmail.com>
2025-06-30 11:54:21 +0300

    Configured

---
+++ Product/src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import { Request , Response } from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/user',(req: Request,res: Response) => {
+    res.json({message: 'Product Service'});
+});
+
+
+app.listen(PORT , () => {
+    console.log(`The user-server is running at http://localhost:${PORT}`);
+    
+});
